78ed003380e0fa10fcc6f8cbc3019c9c296440ff
1569501393 <1569501393@qq.com>
2017-04-19 13:22:04 +0800

    ctw

--- config.inc.php
+++ config.inc.php
@@ -2,7 +2,7 @@
 return array (
   'DB_HOST' => 'localhost',
   'DB_PWD' => '123456',
-  'DB_NAME' => 'msec',
+  'DB_NAME' => 'ctw',
   'DB_USER' => 'root',
   'DB_PORT' => '3306',
   'DB_PREFIX' => 'ctw_',
