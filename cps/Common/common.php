127db8abee0a0610cb7da6bb51fda124466ab3be
1569501393 <1569501393@qq.com>
2017-06-08 20:10:36 +0800

    导出

--- cps/Common/common.php
+++ cps/Common/common.php
@@ -1,4 +1,39 @@
 <?php
+// 导出excel
+function exportExcel($header = array(), $expTitle, $expCellName, $expTableData, $fileName = 'file')
+{
+    $xlsTitle = iconv('utf-8', 'gb2312', $expTitle);//文件名称
+    $fileName = $fileName . date('_YmdHis');//or $xlsTitle 文件名称可根据自己情况设定
+    $cellNum = count($expCellName);
+    $dataNum = count($expTableData);
+
+    vendor("PHPExcel.PHPExcel");
+
+    $objPHPExcel = new \PHPExcel();
+    $cellName = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ', 'AK', 'AL', 'AM', 'AN', 'AO', 'AP', 'AQ', 'AR', 'AS', 'AT', 'AU', 'AV', 'AW', 'AX', 'AY', 'AZ');
+//    $objPHPExcel->getActiveSheet(0)->mergeCells('A1:' . $cellName[$cellNum - 1] . '1');//合并单元格
+    // $objPHPExcel->setActiveSheetIndex(0)->setCellValue('A1', $expTitle.'  Export time:'.date('Y-m-d H:i:s'));
+    for ($i = 0; $i < $cellNum; $i++) {
+        $objPHPExcel->setActiveSheetIndex(0)->setCellValue($cellName[$i] . '1', $expCellName[$i][1]);
+    }
+
+    $objPHPExcel->getActiveSheet()->setTitle($expTitle); //设置工作表名称
+
+    // Miscellaneous glyphs, UTF-8
+    for ($i = 0; $i < $dataNum; $i++) {
+        for ($j = 0; $j < $cellNum; $j++) {
+            $objPHPExcel->getActiveSheet(0)->setCellValue($cellName[$j] . ($i + 2), $expTableData[$i][$expCellName[$j][0]]);
+        }
+    }
+
+    header('pragma:public');
+    header('Content-type:application/vnd.ms-excel;charset=utf-8;name="' . $xlsTitle . '.xls"');
+    header("Content-Disposition:attachment;filename=$fileName.xls");//attachment新窗口打印inline本窗口打印
+    $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+    $objWriter->save('php://output');
+    exit;
+}
+
 // 判断角色  分行和商城直接看角色  支行和客户经理看分行  暂时屏蔽
 function get_role($admin_info) {
 	if ($admin_info['role_id'] == 5) { // 支行
